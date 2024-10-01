95860c28ecb16b7277812cac3a33d86d5d5123b9
inoricode28 <inori237@gmail.com>
2024-10-01 16:41:08 -0500

    Registro_Usuario_Completo

--- Final/app/src/main/java/com/example/afinal/view/ui/RegistroUsuarioActivity.kt
+++ Final/app/src/main/java/com/example/afinal/view/ui/RegistroUsuarioActivity.kt
@@ -1,21 +1,64 @@
 package com.example.afinal.view.ui
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
+import com.example.afinal.databinding.ActivityRegistroUsuarioBinding
+import com.example.afinal.viewmodel.AuthViewModel
+import androidx.lifecycle.Observer
 import com.example.afinal.R
+import com.example.afinal.retrofit.response.RegistroResponse
+import com.example.afinal.util.AppMensaje
+import com.example.afinal.util.TipoMensaje
 
-class RegistroUsuarioActivity : AppCompatActivity() {
+class RegistroUsuarioActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var binding: ActivityRegistroUsuarioBinding
+    private lateinit var authViewModel: AuthViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_registro_usuario)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+                super.onCreate(savedInstanceState)
+        binding = ActivityRegistroUsuarioBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Especificar el tipo explícitamente al instanciar ViewModelProvider
+        authViewModel = ViewModelProvider(this)[AuthViewModel::class.java]
+
+        // Configurar los botones en el listeners
+        binding.btnRegistrar.setOnClickListener(this)
+        binding.btnVolver.setOnClickListener(this)
+
+        // Observador para el loginResponse
+        authViewModel.registroResponse.observe(this, Observer { response ->
+            obtenerDatosRegistro(response)
+        })
+
+    }
+
+    private fun obtenerDatosRegistro(response: RegistroResponse) {
+        binding.btnVolver.isEnabled = true
+        binding.btnRegistrar.isEnabled = true
+        AppMensaje.enviarMensaje(binding.root, response.mensaje, TipoMensaje.ADVERTENCIA)
+    }
+
+    override fun onClick(v: View) {
+        when(v.id){
+            R.id.btnVolver -> startActivity(Intent(applicationContext, MainActivity::class.java))
+            R.id.btnRegistrar -> registrarUsuario()
         }
     }
+
+    private fun registrarUsuario() {
+        binding.btnVolver.isEnabled = false
+        binding.btnRegistrar.isEnabled = false
+        authViewModel.registro(
+            binding.etNombre.text.toString(),
+            binding.etApellido.text.toString(),
+            binding.etCorreo.text.toString(),
+            binding.etCelular.text.toString(),
+            binding.etUser.text.toString(),
+            binding.etContrasena.text.toString())
+    }
+
+
 }
