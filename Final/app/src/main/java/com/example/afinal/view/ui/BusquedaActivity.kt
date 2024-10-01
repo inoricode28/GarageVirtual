0d8dc3d5bbf8d16fb7a96d6138bd711cacdb4381
inoricode28 <inori237@gmail.com>
2024-10-01 11:21:40 -0500

    Principal

--- Final/app/src/main/java/com/example/afinal/view/ui/BusquedaActivity.kt
+++ Final/app/src/main/java/com/example/afinal/view/ui/BusquedaActivity.kt
@@ -1,27 +1,15 @@
 package com.example.afinal.view.ui
 
 import android.os.Bundle
-import android.view.Menu
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.example.afinal.R
+import com.example.afinal.databinding.ActivityBusquedaBinding
 
 class eBusquedaActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityBusquedaBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_busqueda)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-    }
+        binding = ActivityBusquedaBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.principal, menu)
-        return super.onCreateOptionsMenu(menu)
     }
 }
--- Final/app/src/main/java/com/example/afinal/view/ui/PrincipalActivity.kt
+++ Final/app/src/main/java/com/example/afinal/view/ui/PrincipalActivity.kt
@@ -1,15 +1,31 @@
 package com.example.afinal.view.ui
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.example.afinal.R
 import com.example.afinal.databinding.ActivityPrincipalBinding
 
-class PrincipalActivity : AppCompatActivity() {
+
+class PrincipalActivity : AppCompatActivity(), View.OnClickListener  {
     private lateinit var binding: ActivityPrincipalBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityPrincipalBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // Configurar los botones en el listeners
+        binding.btnVehicles.setOnClickListener(this)
+        binding.btnMechanic.setOnClickListener(this)
+        binding.btnMaintenance.setOnClickListener(this)
+    }
+
+    // Funcion que me permite moverme entre vistas
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.btn_vehicles -> startActivity(Intent(applicationContext, eBusquedaActivity::class.java))
+        }
     }
 
 }
